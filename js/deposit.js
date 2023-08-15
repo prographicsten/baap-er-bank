bccb19d7ac4364bcb9051496b42c0d41c9dc0e26
prographicsten <prographicsten@gmail.com>
2023-08-15 20:05:52 +0600

    some code added

--- js/deposit.js
+++ js/deposit.js
@@ -3,13 +3,17 @@ document.getElementById('btn_deposit').addEventListener('click', function () {
     // step 2: get the deposit amount from the deposit input field
     // For input field use .value to the value inside the input field
     const depositField = document.getElementById('deposit_field');
-    const depositAmount = depositField.value;
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
 
     // step 3: get the current deposit total amount
     // for non-input(element other than input, textarea) use .innerText to get the text
     const depositTotalElement = document.getElementById('deposit_total');
-    const depositTotal = depositTotalElement.innerText;
-    depositTotalElement.innerText = depositAmount;
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = currentDepositTotal;
 
     // step 7: clear the deposit field
     depositField.value = '';
